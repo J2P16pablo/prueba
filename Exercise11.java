05372de398a1b93f57952deddadc016a69c38bd1
J2P16pablo <163215923+J2P16pablo@users.noreply.github.com>
2025-08-27 17:17:24 -0600

    Add files via upload

---
+++ Exercise11.java
@@ -0,0 +1,11 @@
+
+package exercise1.pkg1;
+public class Exercise11 {
+public static void main(String[] args) {
+    
+    System.out.println("Welcome to Java, Learning Java Now \n");
+    System.out.println("Programming is fun \n");
+    
+    }
+    
+}
