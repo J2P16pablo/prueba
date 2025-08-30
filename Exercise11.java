2e6b462e7359ee7b2c77842c7171eefc63e220c1
adsjuantq041-rgb <adsjuantq041@gmail.com>
2025-08-29 20:08:48 -0600

    Update Exercise11.java

--- Exercise11.java
+++ Exercise11.java
@@ -5,7 +5,9 @@ public static void main(String[] args) {
     
     System.out.println("Welcome to Java, Learning Java Now \n");
     System.out.println("Programming is fun \n");
+    System.out.println("Programming is fun \n");
     
     }
     
 }
+
